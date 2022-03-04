a2395874f982d0fe8d45251e57b9fe9443ebcc1b
mirumi <info@milmemo.net>
2022-03-04 18:28:26 +0900

    [fix] : download URL

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ function createDownloadLink(type) {
 
   switch (type) {
     case "installer":
-      a.href = "https://common-prd-public-storage.s3.ap-northeast-1.amazonaws.com/zenbu-hiraku_setup.exe"
+      a.href = "https://common-prd-public-storage.s3.ap-northeast-1.amazonaws.com/zh-releases/zenbu-hiraku_setup.exe"
       break
     case "portable_x64":
-      a.href = "https://common-prd-public-storage.s3.ap-northeast-1.amazonaws.com/zenbu-hiraku_x64.exe"
+      a.href = "https://common-prd-public-storage.s3.ap-northeast-1.amazonaws.com/zh-releases/zenbu-hiraku_x64.exe"
       break
     case "portable_x86":
-      a.href = "https://common-prd-public-storage.s3.ap-northeast-1.amazonaws.com/zenbu-hiraku_ia32.exe"
+      a.href = "https://common-prd-public-storage.s3.ap-northeast-1.amazonaws.com/zh-releases/zenbu-hiraku_ia32.exe"
       break
   }
 
